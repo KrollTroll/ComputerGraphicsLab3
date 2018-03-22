1503c0868e9d3c25e865ec4b8d31e14841150fc5
KrollTroll <krollcj@msoe.edu>
2018-03-22 13:29:55 -0500

    line implementation complete
    Signed-off-by: KrollTroll <krollcj@msoe.edu>

--- src/gcontext.cpp
+++ src/gcontext.cpp
@@ -29,14 +29,70 @@ GraphicsContext::~GraphicsContext()
 //TODO: implement
 void GraphicsContext::drawLine(int x0, int y0, int x1, int y1)
 {
+	if(abs(y1-y0) < abs(x1-x0)){
+		if(x0 > x1){
+			lowGradientLine(x1, y1, x0, y0);
+		}
+		else{
+			lowGradientLine(x0, y0, x1, y1);
+		}
+	}
+	else{
+		if(y0 > y1){
+			highGradientLine(x1, y1, x0, y0);
+		}
+		else{
+			highGradientLine(x0, y0, x1, y1);
+		}
+	}
+
 	return;
 }
 
 //TODO: implement
-void GraphicsContext::lowGradientLine(int x0, int y0, int x1, int y1){}
+void GraphicsContext::lowGradientLine(int x0, int y0, int x1, int y1){
+	int dx = x1-x0;
+	int dy = y1-y0;
+	int interceptY = 1;
+	if(dy < 0){
+		interceptY = -1;
+		dy = -dy;
+	}
+	int difference = 2*dy-dx;
+	int y = y0;
+
+	for(int x = x0; x < x1; x++){
+		setPixel(x, y);
+		if(difference > 0){
+			y = y+interceptY;
+			difference = difference -2*dx;
+		}
+		difference = difference + 2*dy;
+	}
+}
 
 //TODO: implement
-void GraphicsContext::highGradientLine(int x0, int y0, int x1, int y1){}
+void GraphicsContext::highGradientLine(int x0, int y0, int x1, int y1){
+	int dx = x1-x0;
+	int dy = y1-y0;
+	int interceptX = 1;
+	interceptX = 1;
+	if(dx < 0){
+		interceptX = -1;
+		dx = -dx;
+	}
+	int difference = 2*dx - dy;
+	int x = x0;
+
+	for(int y = y0; y < y1; y++){
+		setPixel(x, y);
+		if(difference > 0){
+			x = x + interceptX;
+			difference = difference - 2*dy;
+		}
+		difference = difference + 2*dx;
+	}
+}
 
 
 
