acc94b901e25b03c5edd11f3acb677dcb4853369
KrollTroll <krollcj@msoe.edu>
2018-03-26 13:56:46 -0500

    lab complete
    Signed-off-by: KrollTroll <krollcj@msoe.edu>

--- src/gcontext.cpp
+++ src/gcontext.cpp
@@ -15,55 +15,71 @@ GraphicsContext::~GraphicsContext()
 	// here to insure subclasses handle destruction properly
 }
 
-
-/* This is a naive implementation that uses floating-point math
- * and "setPixel" which will need to be provided by the concrete
- * implementation.
- * 
- * Parameters:
- * 	x0, y0 - origin of line
- *  x1, y1 - end of line
- * 
- * Returns: void
+/**
+ * This method draws a line using Bresenham's Line drawing algorithm
+ * @param x0
+ * @param y0 : x0 and y0 are the line origin
+ * @param x1
+ * @param y1 : x1 and y1 are the line terminal point
  */
-//TODO: implement
 void GraphicsContext::drawLine(int x0, int y0, int x1, int y1)
 {
+	//if the change in y is less than the change in x
 	if(abs(y1-y0) < abs(x1-x0)){
+		//if the origin is above the terminal point
 		if(x0 > x1){
+			//draw a low gradient line from terminal to origin
 			lowGradientLine(x1, y1, x0, y0);
 		}
+		//else if the terminal point is above the origin
 		else{
+			//draw a low gradient line from origin to terminal
 			lowGradientLine(x0, y0, x1, y1);
 		}
 	}
+	//else if the change in x is greater than the change in y
 	else{
 		if(y0 > y1){
+			//draw a high gradient line from terminal to origin
 			highGradientLine(x1, y1, x0, y0);
 		}
 		else{
+			//draw a high gradient line from origin to terminal
 			highGradientLine(x0, y0, x1, y1);
 		}
 	}
-
 	return;
 }
 
-//TODO: implement
+/**
+ * draws a low gradient line
+ * 	is a part of the larger line-drawing method using Bresenham's algorithm
+ * @param x0
+ * @param y0 : x0 and y0 are the origin point
+ * @param x1
+ * @param y1 : x1 and y1 are the terminal point
+ */
 void GraphicsContext::lowGradientLine(int x0, int y0, int x1, int y1){
+	//determine slopes
 	int dx = x1-x0;
 	int dy = y1-y0;
+	//set an intercept
 	int interceptY = 1;
+	//if the slope is smaller than 0
 	if(dy < 0){
+		//flip the intercept and slope
 		interceptY = -1;
 		dy = -dy;
 	}
 	int difference = 2*dy-dx;
 	int y = y0;
 
+	//for every x point between the origin and the terminal point
 	for(int x = x0; x < x1; x++){
 		setPixel(x, y);
+		//if the difference is lerger than 0
 		if(difference > 0){
+			//adjust the y and difference values
 			y = y+interceptY;
 			difference = difference -2*dx;
 		}
@@ -71,12 +87,22 @@ void GraphicsContext::lowGradientLine(int x0, int y0, int x1, int y1){
 	}
 }
 
-//TODO: implement
+/**
+ * draws a high gradient line
+ * 	is a part of the larger line-drawing method using Bresenham's algorithm
+ * @param x0
+ * @param y0 : x0 and y0 are the origin point
+ * @param x1
+ * @param y1 : x1 and y1 are the terminal point
+ */
 void GraphicsContext::highGradientLine(int x0, int y0, int x1, int y1){
+	//determine slopes
 	int dx = x1-x0;
 	int dy = y1-y0;
+	//set an intercept
 	int interceptX = 1;
 	interceptX = 1;
+	//if the slope is smaller than 0
 	if(dx < 0){
 		interceptX = -1;
 		dx = -dx;
@@ -84,9 +110,12 @@ void GraphicsContext::highGradientLine(int x0, int y0, int x1, int y1){
 	int difference = 2*dx - dy;
 	int x = x0;
 
+	//for every y point between the origin and terminal
 	for(int y = y0; y < y1; y++){
 		setPixel(x, y);
+		//if the difference is greater than 0
 		if(difference > 0){
+			//adjust the x value and difference value
 			x = x + interceptX;
 			difference = difference - 2*dy;
 		}
@@ -94,27 +123,25 @@ void GraphicsContext::highGradientLine(int x0, int y0, int x1, int y1){
 	}
 }
 
-
-
-/* This is a naive implementation that uses floating-point math
- * and "setPixel" which will need to be provided by the concrete
- * implementation.
- * 
- * Parameters:
- * 	x0, y0 - origin/center of circle
- *  radius - radius of circle
- * 
- * Returns: void
+/**
+ * This is a method that draws a circle using the midpoint algorithm
+ * 	it is an extension of the Bresenham algorithm
+ * @param x0
+ * @param y0 : x0 and y0 are the center point
+ * @param radius :radius of the circle
  */
 void GraphicsContext::drawCircle(int x0, int y0, unsigned int radius)
 {
+	//assign x, y, difference, and error values
 	int x = radius - 1;
 	int y = 0;
 	int differenceX = 1;
 	int differenceY = 1;
 	int error = differenceX - (radius << 1);
 	
+	//as long as the x value is greater or equal to y
 	while(x >= y){
+		//set pixels in a standard octant pattern
 		setPixel(x0 + x, y0 + y);
 		setPixel(x0 + y, y0 + x);
 		setPixel(x0 - y, y0 + x);
@@ -124,13 +151,17 @@ void GraphicsContext::drawCircle(int x0, int y0, unsigned int radius)
 	    setPixel(x0 + y, y0 - x);
 		setPixel(x0 + x, y0 - y);
 
+		//if the error value falls below 0
 		if(error <= 0){
+			//adjust y and y difference values
 			y++;
 			error += differenceY;
 			differenceY += 2;
 		}
 
+		//if the error falls above 0
 		if(error > 0){
+			//adjust the x and x difference values
 			x--;
 			differenceX += 2;
 			error += differenceX - (radius << 1);
--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,27 @@ int main(void)
  
 	// draw some lines
 	gc->setColor(GraphicsContext::GREEN);
-	gc->setPixel(10,10);
-	gc->setPixel(30,30);
-	gc->drawLine(100,100,100,500);
+	gc->drawLine(-100,-75,800,600);
 	gc->setColor(GraphicsContext::RED);
-	gc->drawLine(100,500,500,500);
+	gc->drawLine(800,0,0, 600);
 	gc->setColor(GraphicsContext::BLUE);
-	gc->drawLine(500,500,500,100);
-	gc->setColor(GraphicsContext::YELLOW);
-	gc->drawLine(500,100,100,100);
+	gc->drawCircle(400,300,150);
+	gc->setColor(GraphicsContext::GREEN);
+	gc->drawLine(400,0,400,600);
+	gc->setColor(GraphicsContext::RED);
+	gc->drawLine(0,300,800,300);
 	gc->setColor(GraphicsContext::MAGENTA);
-	gc->drawCircle(300,300,200);
+	gc->drawCircle(0,0,200);
+	gc->drawCircle(800, 0, 200);
+	gc->drawCircle(0, 600, 200);
+	gc->drawCircle(800, 600, 200);
+	gc->setColor(GraphicsContext::YELLOW);
+	gc->drawCircle(400, 300, 300);
+	gc->drawCircle(400, -200, 300);
+	gc->drawCircle(400, 800, 300);
+	gc->setColor(GraphicsContext::BLUE);
+	gc->drawCircle(-200, 300, 400);
+	gc->drawCircle(1000, 300, 400);
 	sleep(5);
  
 	delete gc;
